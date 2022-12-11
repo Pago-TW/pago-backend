09e34d42f6d1d582353f52d777e9490807f7007a
Shiun0413 <a0923183408@gmail.com>
2022-12-12 04:23:28 +0800

    chore: set spring boot environment

---
+++ pago-backend/src/main/java/tw/pago/pagobackend/PagoBackendApplication.java
@@ -0,0 +1,13 @@
+package tw.pago.pagobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagoBackendApplication.class, args);
+    }
+
+}
---
+++ pago-backend/src/test/java/tw/pago/pagobackend/PagoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.pago.pagobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
