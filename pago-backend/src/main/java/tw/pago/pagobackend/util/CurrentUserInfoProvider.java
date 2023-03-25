1ba18e1d69cdf8a0745bd11622a49c9ffcc888db
Shiun0413 <a0923183408@gmail.com>
2023-03-26 00:47:49 +0800

    feat(Util): add CurrentUserInfoProvider

---
+++ pago-backend/src/main/java/tw/pago/pagobackend/util/CurrentUserInfoProvider.java
@@ -0,0 +1,29 @@
+package tw.pago.pagobackend.util;
+
+import org.springframework.stereotype.Component;
+import tw.pago.pagobackend.model.User;
+import tw.pago.pagobackend.security.AuthenticationFacade;
+import tw.pago.pagobackend.security.model.UserPrincipal;
+import tw.pago.pagobackend.service.UserService;
+
+@Component
+public class CurrentUserInfoProvider {
+
+  private final UserService userService;
+  private final AuthenticationFacade authenticationFacade;
+
+  public CurrentUserInfoProvider(UserService userService, AuthenticationFacade authenticationFacade) {
+    this.userService = userService;
+    this.authenticationFacade = authenticationFacade;
+  }
+
+  public String getCurrentLoginUserId() {
+    UserPrincipal userPrincipal = (UserPrincipal) authenticationFacade.getAuthentication().getPrincipal();
+    return userPrincipal.getId();
+  }
+
+  public User getCurrentLoginUser() {
+    String userId = getCurrentLoginUserId();
+    return userService.getUserById(userId);
+  }
+}
