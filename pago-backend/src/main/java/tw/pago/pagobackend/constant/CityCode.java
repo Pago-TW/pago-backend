5acae20c8a96dbfd2fcf7329162c6a987e8a0bf4
Shiun0413 <a0923183408@gmail.com>
2023-03-29 00:10:06 +0800

    feat(Util): add countryCode dependency & add CityCode Enum & add City&Country Utils

---
+++ pago-backend/src/main/java/tw/pago/pagobackend/constant/CityCode.java
@@ -0,0 +1,35 @@
+package tw.pago.pagobackend.constant;
+
+
+import lombok.Getter;
+
+@Getter
+public enum CityCode {
+  TPE("台北", "Taipei"),
+  TKO("東京", "Tokyo"),
+  PUS("釜山", "Busan")
+
+
+
+  
+
+  ;
+
+  private String chineseName;
+  private String englishName;
+
+  CityCode(String chineseName, String englishName) {
+    this.chineseName = chineseName;
+    this.englishName = englishName;
+  }
+
+  public static CityCode getEnumByEnglishName(String englishName) {
+    for (CityCode cityCode : values()) {
+      if (cityCode.getEnglishName().equalsIgnoreCase(englishName)) {
+        return cityCode;
+      }
+    }
+    throw new IllegalArgumentException("No CityCodeEnum found for English name: " + englishName);
+  }
+
+}
