7c9ea49936a89326579cdb0e00ddf1ce6c3c837c
Shiun0413 <a0923183408@gmail.com>
2023-04-01 19:55:21 +0800

    feat(OAuth): update configuration & add HTML for triggering APIs

--- pago-backend/src/main/java/tw/pago/pagobackend/dto/GoogleOAuthRequestDto.java
+++ pago-backend/src/main/java/tw/pago/pagobackend/dto/GoogleOAuthRequestDto.java
@@ -1,2 +1,23 @@
-package tw.pago.pagobackend.dto;public class GoogleOAuthRequestDto {
+package tw.pago.pagobackend.dto;
+
+public class GoogleOAuthRequestDto {
+
+  private String code;
+  private String state;
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
 }
