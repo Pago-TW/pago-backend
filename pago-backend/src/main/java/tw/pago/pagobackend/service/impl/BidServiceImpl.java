026865f3aacb70dd575874ce5853d9dc38d9ef88
ycday <chennyycday@gmail.com>
2023-04-09 18:17:26 +0800

    feat(Bid): SES on bid creation

--- pago-backend/src/main/java/tw/pago/pagobackend/service/impl/BidServiceImpl.java
+++ pago-backend/src/main/java/tw/pago/pagobackend/service/impl/BidServiceImpl.java
@@ -1,5 +1,7 @@
 package tw.pago.pagobackend.service.impl;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.BeanUtils;
@@ -12,17 +14,22 @@ import tw.pago.pagobackend.dao.BidDao;
 import tw.pago.pagobackend.dto.BidCreatorReviewDto;
 import tw.pago.pagobackend.dto.BidResponseDto;
 import tw.pago.pagobackend.dto.CreateBidRequestDto;
+import tw.pago.pagobackend.dto.EmailRequestDto;
 import tw.pago.pagobackend.dto.ListQueryParametersDto;
 import tw.pago.pagobackend.dto.ReviewRatingResultDto;
 import tw.pago.pagobackend.dto.UpdateBidRequestDto;
 import tw.pago.pagobackend.exception.ResourceNotFoundException;
 import tw.pago.pagobackend.model.Bid;
+import tw.pago.pagobackend.model.Order;
 import tw.pago.pagobackend.model.Trip;
 import tw.pago.pagobackend.model.User;
 import tw.pago.pagobackend.service.BidService;
+import tw.pago.pagobackend.service.OrderService;
 import tw.pago.pagobackend.service.ReviewService;
+import tw.pago.pagobackend.service.SesEmailService;
 import tw.pago.pagobackend.service.TripService;
 import tw.pago.pagobackend.service.UserService;
+import tw.pago.pagobackend.util.CurrentUserInfoProvider;
 import tw.pago.pagobackend.util.EntityPropertyUtil;
 import tw.pago.pagobackend.util.UuidGenerator;
 
@@ -35,6 +42,8 @@ public class BidServiceImpl implements BidService {
   @Autowired
   private UuidGenerator uuidGenerator;
   @Autowired
+  private OrderService orderService;
+  @Autowired
   private TripService tripService;
   @Autowired
   private UserService userService;
@@ -42,9 +51,16 @@ public class BidServiceImpl implements BidService {
   private ReviewService reviewService;
   @Autowired
   private BidAssembler bidAssembler;
+  @Autowired
+  private SesEmailService sesEmailService;
+  @Autowired
+  private CurrentUserInfoProvider currentUserInfoProvider;
 
   @Override
   public Bid createBid(CreateBidRequestDto createBidRequestDto) {
+
+    String currency = createBidRequestDto.getCurrency().toString();
+    String bidAmount = createBidRequestDto.getBidAmount().toString();
 
     // Init UUID & Set UUID
     String uuid = uuidGenerator.getUuid();
@@ -53,6 +69,38 @@ public class BidServiceImpl implements BidService {
     // Create bid
     bidDao.createBid(createBidRequestDto);
     Bid bid = bidDao.getBidById(uuid);
+
+    // Get bidder's name
+    String bidderName = currentUserInfoProvider.getCurrentLoginUser().getFirstName();
+
+    // Get the order creator's email
+    String orderId = createBidRequestDto.getOrderId();
+    Order order = orderService.getOrderById(orderId);
+    String orderCreatorId = order.getConsumerId();
+    User orderCreator = userService.getUserById(orderCreatorId);
+    String orderCreatorEmail = orderCreator.getEmail();
+
+    // Get the order item name
+    String orderItemName = order.getOrderItem().getName();
+    // Get the user name
+    String orderCreatorName = orderCreator.getFirstName();
+    // Get current date
+    Date now = new Date();
+    String date = new SimpleDateFormat("yyyy-MM-dd").format(now);
+
+    String emailBody = String.format("親愛的%s您好，感謝您使用Pago的服務\n" +
+    "%s已於%s在您的訂單 %s 出價：%s%s", 
+    orderCreatorName, bidderName, date, orderItemName, currency, bidAmount);
+
+    // Prepare the email content
+    EmailRequestDto emailRequest = new EmailRequestDto();
+    emailRequest.setTo(orderCreatorEmail);
+    emailRequest.setSubject("【Pago 訂單出價通知】" + orderItemName);
+    emailRequest.setBody(emailBody);
+
+    // Send the email notification
+    sesEmailService.sendEmail(emailRequest);
+    System.out.println("......Email sent!");
 
     return bid;
   }
