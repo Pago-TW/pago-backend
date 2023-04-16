d9b6761d5c69b0cbce936ea8a680bfef5ea3d4b9
Shiun0413 <a0923183408@gmail.com>
2023-04-16 15:27:36 +0800

    feat(Currency): add more currency

--- pago-backend/src/main/java/tw/pago/pagobackend/constant/CurrencyEnum.java
+++ pago-backend/src/main/java/tw/pago/pagobackend/constant/CurrencyEnum.java
@@ -1,6 +1,31 @@
 package tw.pago.pagobackend.constant;
 
 public enum CurrencyEnum {
-  USD,
-  TWD
+  USD, // 美金
+  EUR, // 歐元
+  GBP, // 英鎊
+  JPY, // 日圓
+  AUD, // 澳元
+  CAD, // 加元
+  CHF, // 瑞士法郎
+  CNY, // 人民幣
+  HKD, // 港幣
+  INR, // 印度盧比
+  KRW, // 韓元
+  MXN, // 墨西哥比索
+  NZD, // 紐西蘭元
+  RUB, // 俄羅斯盧布
+  SGD, // 新加坡元
+  ZAR, // 南非蘭特
+  SEK, // 瑞典克朗
+  NOK, // 挪威克朗
+  DKK, // 丹麥克朗
+  BRL, // 巴西里拉
+  AED, // 阿聯酋迪拉姆
+  IDR, // 印尼盧比
+  THB, // 泰銖
+  MYR, // 馬來西亞林吉特
+  PHP, // 菲律賓披索
+  TRY  // 土耳其里拉
 }
+
