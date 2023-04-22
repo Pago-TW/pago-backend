d41a4f8d7e5009a8d9d90171ebaac42ee21bd794
Shiun0413 <a0923183408@gmail.com>
2023-04-23 03:29:28 +0800

    refactor(File): refactor upload file API

--- pago-backend/src/main/java/tw/pago/pagobackend/controller/FileController.java
+++ pago-backend/src/main/java/tw/pago/pagobackend/controller/FileController.java
@@ -1,87 +1,96 @@
-// package tw.pago.pagobackend.controller;
+ package tw.pago.pagobackend.controller;
 
-// import java.io.IOException;
-// import java.net.URL;
+ import java.net.URL;
+ import java.util.List;
+ import lombok.AllArgsConstructor;
+ import org.springframework.http.HttpStatus;
+ import org.springframework.http.ResponseEntity;
+ import org.springframework.web.bind.annotation.PostMapping;
+ import org.springframework.web.bind.annotation.RequestParam;
+ import org.springframework.web.bind.annotation.RestController;
+ import org.springframework.web.multipart.MultipartFile;
+ import tw.pago.pagobackend.dto.CreateFileRequestDto;
+ import tw.pago.pagobackend.service.FileService;
+ import tw.pago.pagobackend.util.CurrentUserInfoProvider;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PatchMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
+ @RestController
+ @AllArgsConstructor
+ public class FileController {
 
-// import org.springframework.web.multipart.MultipartFile;
-
-// import com.amazonaws.services.s3.AmazonS3;
-// import com.amazonaws.services.s3.AmazonS3Client;
-// import com.amazonaws.services.s3.model.AmazonS3Exception;
-// import com.amazonaws.services.s3.model.ObjectMetadata;
-
-// import tw.pago.pagobackend.config.AwsS3Config;
-// import tw.pago.pagobackend.model.File;
-// import tw.pago.pagobackend.service.FileService;
-
-// @RestController
-// public class FileController {
-//     @Autowired
-//     private FileService fileService;
+     private final FileService fileService;
+     private final CurrentUserInfoProvider currentUserInfoProvider;
     
-//     // @PostMapping("/files")
-//     // public ResponseEntity<URL> uploadFile(@RequestParam("file") MultipartFile file) {
+     // @PostMapping("/files")
+     // public ResponseEntity<URL> uploadFile(@RequestParam("file") MultipartFile file) {
 
-//     //     try {
-//     //         String fileName = file.getOriginalFilename();
-//     //         ObjectMetadata metadata = new ObjectMetadata();
-//     //         metadata.setContentType(file.getContentType());
-//     //         metadata.setContentLength(file.getSize());
+     //     try {
+     //         String fileName = file.getOriginalFilename();
+     //         ObjectMetadata metadata = new ObjectMetadata();
+     //         metadata.setContentType(file.getContentType());
+     //         metadata.setContentLength(file.getSize());
 
-//     //         amazonS3.putObject(BUCKET_NAME, fileName, file.getInputStream(), metadata);
-//     //         System.out.println(2);
-//     //         URL resUrl = amazonS3.getUrl(BUCKET_NAME, fileName);
-//     //         return ResponseEntity.status(HttpStatus.OK).body(resUrl);
-//     //     } catch (AmazonS3Exception e) {
-//     //         // Log the error message and status code
-//     //         System.err.println("Amazon S3 error: " + e.getErrorCode());
-//     //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-//     //     } catch (Exception e) {
-//     //         // Log the error message and status code
-//     //         System.err.println("Unknown error: " + e.getMessage());
-//     //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-//     //     } 
-//     // }
+     //         amazonS3.putObject(BUCKET_NAME, fileName, file.getInputStream(), metadata);
+     //         System.out.println(2);
+     //         URL resUrl = amazonS3.getUrl(BUCKET_NAME, fileName);
+     //         return ResponseEntity.status(HttpStatus.OK).body(resUrl);
+     //     } catch (AmazonS3Exception e) {
+     //         // Log the error message and status code
+     //         System.err.println("Amazon S3 error: " + e.getErrorCode());
+     //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+     //     } catch (Exception e) {
+     //         // Log the error message and status code
+     //         System.err.println("Unknown error: " + e.getMessage());
+     //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+     //     }
+     // }
+
+
+   @PostMapping("/files")
+   public ResponseEntity<List<URL>> uploadFile(
+       @RequestParam("file") List<MultipartFile> files,
+       @RequestParam("objectId") String objectId,
+       @RequestParam("objectType") String objectType) {
+
+     String currentLoginUserId = currentUserInfoProvider.getCurrentLoginUserId();
+
+     CreateFileRequestDto createFileRequestDto = new CreateFileRequestDto();
+     createFileRequestDto.setFileCreator(currentLoginUserId);
+     createFileRequestDto.setObjectId(objectId);
+     createFileRequestDto.setObjectType(objectType);
+
+     List<URL> fileList =  fileService.uploadFile(files, createFileRequestDto);
+
+     return ResponseEntity.status(HttpStatus.OK).body(fileList);
+   }
+
     
 //     @PostMapping("/users/{userId}/files")
 //     public ResponseEntity<URL> uploadFile(@PathVariable String userId, @RequestParam("file") MultipartFile file) {
-//         URL resUrl = fileService.uploadFile(userId, file);  
+//         URL resUrl = fileService.uploadFile(userId, file);
 //         return ResponseEntity.status(HttpStatus.OK).body(resUrl);
 //     }
     
     
-//     // @GetMapping ("/files/get")
-//     // public ResponseEntity<URL> getFile() {
-//     //     System.out.println(1);
-//     //     URL resUrl = amazonS3.getUrl(BUCKET_NAME, "425de133-dae7-425f-852f-6b8fd03a030a.jpeg");
-//     //     System.out.println(2);
-//     //     amazonS3.getObject(BUCKET_NAME, "425de133-dae7-425f-852f-6b8fd03a030a.jpeg");
-//     //     System.out.println(3);
-//     //     return ResponseEntity.status(HttpStatus.OK).body(resUrl);
-//     // }
+     // @GetMapping ("/files/get")
+     // public ResponseEntity<URL> getFile() {
+     //     System.out.println(1);
+     //     URL resUrl = amazonS3.getUrl(BUCKET_NAME, "425de133-dae7-425f-852f-6b8fd03a030a.jpeg");
+     //     System.out.println(2);
+     //     amazonS3.getObject(BUCKET_NAME, "425de133-dae7-425f-852f-6b8fd03a030a.jpeg");
+     //     System.out.println(3);
+     //     return ResponseEntity.status(HttpStatus.OK).body(resUrl);
+     // }
 
-//     // @GetMapping ("/files/get")
-//     // public String getFile() {
-//     //     try {
-//     //         return testvar;
-//     //     } catch (Exception e) {
-//     //         return "error";
-//     //     }
+     // @GetMapping ("/files/get")
+     // public String getFile() {
+     //     try {
+     //         return testvar;
+     //     } catch (Exception e) {
+     //         return "error";
+     //     }
         
-//     // }
-// }
+     // }
+ }
 
 
 
