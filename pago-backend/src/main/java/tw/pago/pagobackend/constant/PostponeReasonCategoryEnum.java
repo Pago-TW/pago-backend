13f59df8cd891039e00f6719dc13ab8a79fb1541
ycday <chennyycday@gmail.com>
2023-04-23 11:08:29 +0800

    feat(PostponeReasonCategoryEnum): add description

--- pago-backend/src/main/java/tw/pago/pagobackend/constant/PostponeReasonCategoryEnum.java
+++ pago-backend/src/main/java/tw/pago/pagobackend/constant/PostponeReasonCategoryEnum.java
@@ -1,7 +1,17 @@
 package tw.pago.pagobackend.constant;
 
 public enum PostponeReasonCategoryEnum {
-  FORCE_MAJEURE, // 不可抗力
-  PERSONAL_FACTOR, // 個人因素
-  OTHER // 其他
+    FORCE_MAJEURE("不可抗力"),
+    PERSONAL_FACTOR("個人因素"),
+    OTHER("其他");
+
+    private final String description;
+
+    PostponeReasonCategoryEnum(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
