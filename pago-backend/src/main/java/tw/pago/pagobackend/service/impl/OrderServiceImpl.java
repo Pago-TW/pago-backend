c2c53b17e0ee00a1e0e658986f99df08df6b8cc1
ycday <chennyycday@gmail.com>
2023-05-01 15:55:46 +0800

    fix(OrderServiceImpl): fix SES format

--- pago-backend/src/main/java/tw/pago/pagobackend/service/impl/OrderServiceImpl.java
+++ pago-backend/src/main/java/tw/pago/pagobackend/service/impl/OrderServiceImpl.java
@@ -1098,9 +1098,9 @@ public class OrderServiceImpl implements OrderService {
     String postponeNote = postponeRecord.getNote();
     String noteLine = (postponeNote != null) ? String.format("延期原因描述：%s", postponeNote) : "";
     
-    String emailBody = String.format("親愛的%s您好，感謝您使用Pago的服務<br>" +
-        "於%s，%s想要為「%s」的訂單申請延期，請至Pago網站確認並回覆。<br><br>" +
-        "以下為%s申請延期之詳細資訊：<br>" +
+    String emailBody = String.format("親愛的 %s 您好，感謝您使用 Pago 的服務<br>" +
+        "於 %s ， %s 想要為「%s」的訂單申請延期，請至 Pago 網站確認並回覆。<br><br>" +
+        "以下為 %s 申請延期之詳細資訊：<br>" +
         "延期原因：%s<br>%s",
         username, date, postponeRequesterFirstName, orderItemName, postponeRequesterFirstName, postponeRecord.getPostponeReason().getDescription(), noteLine);
 
@@ -1151,9 +1151,9 @@ public class OrderServiceImpl implements OrderService {
     String cancelNote = cancellationRecord.getNote();
     String noteLine = (cancelNote != null) ? String.format("取消原因描述：%s", cancelNote) : "";
     
-    String emailBody = String.format("親愛的%s您好，感謝您使用Pago的服務<br>" +
-        "於%s，%s想要將「%s」的訂單取消，請至Pago網站確認並回覆。<br><br>" +
-        "以下為%s申請取消之詳細資訊：<br>" +
+    String emailBody = String.format("親愛的 %s 您好，感謝您使用 Pago 的服務<br>" +
+        "於 %s ， %s 想要將「%s」的訂單取消，請至 Pago 網站確認並回覆。<br><br>" +
+        "以下為 %s 申請取消之詳細資訊：<br>" +
         "取消原因：%s<br>%s",
         username, date, cancelRequesterFirstName, orderItemName, cancelRequesterFirstName, cancellationRecord.getCancelReason().getDescription(), noteLine);
 
@@ -1185,8 +1185,8 @@ public class OrderServiceImpl implements OrderService {
     Date now = new Date();
     String date = new SimpleDateFormat("yyyy-MM-dd").format(now);
 
-    String emailBody = String.format("親愛的%s您好，感謝您使用Pago的服務<br>" +
-        "您的訂單 %s，已於%s更新為「%s」", username, orderItemName, date,
+    String emailBody = String.format("親愛的 %s 您好，感謝您使用 Pago 的服務<br>" +
+        "您的訂單 %s ，已於 %s 更新為「%s」", username, orderItemName, date,
         updateOrderAndOrderItemRequestDto.getOrderStatus().getDescription());
 
     // Prepare the email content
@@ -1216,8 +1216,8 @@ public class OrderServiceImpl implements OrderService {
   
     String result = updatePostponeRecordRequestDto.getIsPostponed() ? "通過" : "被拒絕";
   
-    String emailBody = String.format("親愛的%s您好，感謝您使用Pago的服務<br>" +
-        "於%s，「%s」的訂單延期申請已%s。現在的訂單狀態為%s，請至Pago網站進行確認並查看詳情<br>",
+    String emailBody = String.format("親愛的 %s 您好，感謝您使用 Pago 的服務<br>" +
+        "於 %s，「%s」的訂單延期申請已%s。現在的訂單狀態為 %s，請至 Pago 網站進行確認並查看詳情<br>",
         username, date, orderItemName, result, updatedOrderStatus);
   
     // Prepare the email content
@@ -1247,8 +1247,8 @@ public class OrderServiceImpl implements OrderService {
 
     String result = updateCancellationRecordRequestDto.getIsCancelled() ? "通過" : "被拒絕";
 
-    String emailBody = String.format("親愛的%s您好，感謝您使用Pago的服務<br>" +
-            "於%s，「%s」的訂單取消申請已%s。現在的訂單狀態為%s，請至Pago網站進行確認並查看詳情<br>",
+    String emailBody = String.format("親愛的 %s 您好，感謝您使用 Pago 的服務<br>" +
+            "於 %s ，「%s」的訂單取消申請已%s。現在的訂單狀態為 %s ，請至 Pago 網站進行確認並查看詳情<br>",
             username, date, orderItemName, result, updatedOrderStatus);
 
     // Prepare the email content
