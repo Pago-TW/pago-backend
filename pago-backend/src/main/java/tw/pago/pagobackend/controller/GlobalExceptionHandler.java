c6c12409a8a10c0780616a2f394f8c0a36d9868e
ycday <chennyycday@gmail.com>
2023-05-08 11:39:48 +0800

    chore(GlobalExceptionHandler): removed exceptionhandler for error code 500 in order to see error logs

--- pago-backend/src/main/java/tw/pago/pagobackend/controller/GlobalExceptionHandler.java
+++ pago-backend/src/main/java/tw/pago/pagobackend/controller/GlobalExceptionHandler.java
@@ -66,16 +66,7 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleGenericException(Exception ex) {
-        Map<String, Object> errorDetails = new HashMap<>();
-        errorDetails.put("timestamp", LocalDateTime.now());
-        errorDetails.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        errorDetails.put("error", "Internal Server Error");
-        errorDetails.put("message", ex.getMessage());
 
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException ex) {
