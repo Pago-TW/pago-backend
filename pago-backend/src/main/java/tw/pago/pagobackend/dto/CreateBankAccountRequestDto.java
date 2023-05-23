22df45f743c67d644a1f5fab9cb551111a38c2ac
Shiun0413 <a0923183408@gmail.com>
2023-05-24 00:04:37 +0800

    feat(POST /bank-accounts): validate json form data by using @NotBlank in CreateBankAccountRequestDto

--- pago-backend/src/main/java/tw/pago/pagobackend/dto/CreateBankAccountRequestDto.java
+++ pago-backend/src/main/java/tw/pago/pagobackend/dto/CreateBankAccountRequestDto.java
@@ -2,6 +2,7 @@ package tw.pago.pagobackend.dto;
 
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import javax.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,14 +11,18 @@ import lombok.NoArgsConstructor;
 public class CreateBankAccountRequestDto {
   private String bankAccountId;
   private String userId;
+  @NotBlank
   private String legalName;
-  private LocalDate birthDate;
-  private String identityNumber;
-  private String residentialAddress;
+  private LocalDate birthDate; // TODO 跟前端討論會傳入的 date pattern
+  @NotBlank
   private String zipCode;
+  @NotBlank
   private String bankCode;
+  @NotBlank
   private String branchCode;
+  @NotBlank
   private String accountHolderName;
+  @NotBlank
   private String accountNumber;
   private ZonedDateTime createDate;
   private ZonedDateTime updateDate;
