332b27c58d5011cbda748d4003a104a872e7ede0
Shiun0413 <a0923183408@gmail.com>
2023-05-23 16:00:57 +0800

    refactor(UpdateUserRequestDto): remove @NotBlankIfPresent on aboutMe

--- pago-backend/src/main/java/tw/pago/pagobackend/dto/UpdateUserRequestDto.java
+++ pago-backend/src/main/java/tw/pago/pagobackend/dto/UpdateUserRequestDto.java
@@ -35,7 +35,6 @@ public class UpdateUserRequestDto {
   private String googleId;
   private AccountStatusEnum accountStatus;
   private Date updateDate;
-  @NotBlankIfPresent
   private String aboutMe;
 
   private String country;
