37a7aee7e4023edb9824bc3c4a14d4ccb19d1afb
Shiun0413 <a0923183408@gmail.com>
2023-09-13 01:07:07 +0800

    feat(ReviewResponseDto): add 'isTraveling' property
    The new 'isTraveling' property indicates whether the user is currently on a trip.

--- pago-backend/src/main/java/tw/pago/pagobackend/assembler/ReviewAssembler.java
+++ pago-backend/src/main/java/tw/pago/pagobackend/assembler/ReviewAssembler.java
@@ -2,7 +2,6 @@ package tw.pago.pagobackend.assembler;
 
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import tw.pago.pagobackend.dto.ReviewCreatorDto;
 import tw.pago.pagobackend.dto.ReviewOrderDto;
@@ -12,6 +11,7 @@ import tw.pago.pagobackend.model.Order;
 import tw.pago.pagobackend.model.Review;
 import tw.pago.pagobackend.model.User;
 import tw.pago.pagobackend.service.OrderService;
+import tw.pago.pagobackend.service.TripService;
 import tw.pago.pagobackend.service.UserService;
 
 @Component
@@ -21,6 +21,7 @@ public class ReviewAssembler implements Assembler<Review, ReviewResponseDto> {
     private final ModelMapper modelMapper;
     private final UserService userService;
     private final OrderService orderService;
+    private final TripService tripService;
 
     @Override
     public ReviewResponseDto assemble(Review review) {
@@ -33,6 +34,10 @@ public class ReviewAssembler implements Assembler<Review, ReviewResponseDto> {
       ReviewOrderDto orderDto = modelMapper.map(order, ReviewOrderDto.class);
 
       ReviewResponseDto reviewResponseDto = modelMapper.map(review, ReviewResponseDto.class);
+
+      // TODO 效能瓶頸，重複查詢，例如：我有 10 筆 Review 都是 userA 創建的，所以我們其實只需要查詢一次資料庫知道 userA 是否正在旅途中，而不是因為有 10 筆 Review 都是 userA 創建，導致我每次查詢 review 都會多查一次userA 是否正在旅途中
+      boolean isReviewCreatorTraveling = tripService.isUserTraveling(creator.getUserId());
+      creatorDto.setTraveling(isReviewCreatorTraveling);
 
       reviewResponseDto.setCreator(creatorDto);
       reviewResponseDto.setTarget(targetDto);
